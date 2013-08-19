0075a11c944eb4c4a2409ec1dd2566ed72d565ea
Luke Matthew Sutton <me@lukematthewsutton.com>
2013-08-19 14:01:16 +0930

    Move SubmissionError into it's own file.

--- lib/spook_and_pay.rb
+++ lib/spook_and_pay.rb
@@ -11,6 +11,7 @@ require 'braintree'
 
 require 'spook_and_pay/helpers'
 require 'spook_and_pay/credit_card'
+require 'spook_and_pay/submission_error'
 require 'spook_and_pay/result'
 require 'spook_and_pay/transaction'
 require 'spook_and_pay/adapters'
